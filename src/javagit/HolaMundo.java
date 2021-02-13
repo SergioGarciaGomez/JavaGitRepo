127564b223f1443ab547b483bd73041f3d7b5233
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-02-13 17:53:19 +0100

    Segundo commit local

--- src/javagit/HolaMundo.java
+++ src/javagit/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
   public static void main(String[] args) {
     System.out.println("Hola mundo.");
     System.out.println("Este es mi primer commit local.");
+    System.out.println("Este es el segundo commit local");
   }
 
 }
