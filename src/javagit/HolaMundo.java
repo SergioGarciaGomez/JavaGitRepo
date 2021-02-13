77c8ccd0ffb87bfd6e7170b5be4f09fd4a569e4c
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-02-13 17:17:56 +0100

    Primer commit local

---
+++ src/javagit/HolaMundo.java
@@ -0,0 +1,10 @@
+package javagit;
+
+public class HolaMundo {
+
+  public static void main(String[] args) {
+    System.out.println("Hola mundo.");
+    System.out.println("Este es mi primer commit local.");
+  }
+
+}
